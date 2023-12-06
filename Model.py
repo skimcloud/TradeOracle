eca26fdd4f34d14aa0c8ba151f9d1411fe0c11b0
skimcloud <113874029+skimcloud@users.noreply.github.com>
2023-12-05 21:27:46 -0800

    More Stats

--- Model.py
+++ Model.py
@@ -7,7 +7,7 @@ from xgboost import plot_tree
 import seaborn as sns
 
 # Load the dataset
-data = pd.read_csv('processed_dataset.csv')
+data = pd.read_csv('final_indicators_with_orders.csv')
 
 # Define features and target variable
 features = data.drop('success', axis=1)
--- ViewPerformanceStats.py
+++ ViewPerformanceStats.py
@@ -26,11 +26,14 @@ for _, out_order in out_orders.iterrows():
 
 # Calculate average exit price for each trade
 for trade_key, exit_prices in exits.items():
-    exit_prices = [float(exit_price) for exit_price in exit_prices]  # Convert exit prices to float
+    exit_prices = [float(exit_price) for exit_price in exit_prices if pd.notnull(exit_price)]  # Convert exit prices to float
     exits[trade_key] = sum(exit_prices) / len(exit_prices) if len(exit_prices) > 0 else None  # Mark as None if no exit
 
-# Calculate instantaneous profitable trade rate over time
+# Calculate instantaneous profitable trade rate over time and profit over time
 instantaneous_win_rates = []
+profit_over_time = []
+total_trades = 0
+total_profit = 0
 for _, in_order in in_orders.iterrows():
     trade_key = (
         in_order['trader'],
@@ -38,40 +41,108 @@ for _, in_order in in_orders.iterrows():
         in_order['expiration'],
         in_order['contract_details']
     )
-    six_months_ago = datetime.now() - timedelta(days=90)  # 6 months ago
+    three_months_ago = datetime.now() - timedelta(days=90)  # 3 months ago
     execution_datetime = pd.to_datetime(in_order['order_execution_datetime'])
     if trade_key in exits:
         entry_price = float(in_order['contract_price'])  # Convert entry price to float
         exit_price = exits[trade_key]
         is_profitable = exit_price is not None and exit_price > entry_price
         instantaneous_win_rates.append((pd.to_datetime(in_order['order_execution_datetime']), is_profitable))
-    elif execution_datetime < six_months_ago:
+
+        if is_profitable:
+            total_profit += exit_price - entry_price  # Calculate profit
+        else:
+            total_profit -= (entry_price * 0.8)  # Apply 80% stop loss on losing positions
+        profit_over_time.append((execution_datetime, total_profit))  # Update profit over time
+        total_trades += 1
+    elif execution_datetime < three_months_ago:
         is_profitable = False
         instantaneous_win_rates.append((pd.to_datetime(in_order['order_execution_datetime']), is_profitable))
 
+# Calculate consecutive wins
+consecutive_wins = 0
+max_consecutive_wins = 0
+for _, is_win in instantaneous_win_rates:
+    if is_win:
+        consecutive_wins += 1
+        max_consecutive_wins = max(max_consecutive_wins, consecutive_wins)
+    else:
+        consecutive_wins = 0
 
+# Calculate the average number of consecutive wins
+average_consecutive_wins = max_consecutive_wins / len(instantaneous_win_rates) if len(instantaneous_win_rates) > 0 else 0
+print(f"Average number of consecutive wins: {average_consecutive_wins:.2f}")
 
 # Sort trades by datetime
 instantaneous_win_rates.sort(key=lambda x: x[0])
+profit_over_time.sort(key=lambda x: x[0])
 
 # Calculate cumulative profitable trade rate over time
 dates, rates = zip(*instantaneous_win_rates)
 cumulative_rates = [sum(rates[:i + 1]) / (i + 1) for i in range(len(rates))]
 
-# Calculate the current profitability ratio
-current_profit_ratio = cumulative_rates[-1]
+# Calculate the average return per winning trade
+average_return_per_winning_trade = total_profit / total_trades if total_trades > 0 else 0
+print(f"Average return per winning trade: {average_return_per_winning_trade * 100:.2f}%")
 
-# Print the current profitability ratio
-print("Current Profitability Ratio:", current_profit_ratio)
+# Calculate cumulative profit over time
+dates_profit, profits = zip(*profit_over_time)
 
-# Plotting the cumulative profitable trade rate over time
-plt.figure(figsize=(10, 6))
-plt.plot(dates, cumulative_rates)
-plt.xlabel('Time')
-plt.ylabel('Profitability Ratio (Considering Expirations)')
-plt.title('MariaC82\'s Profitability Ratio Over Time')
-plt.xticks(rotation=45)
-plt.yticks([i/100 for i in range(0, 101, 5)])  # Adjust range and step size as needed (100 for 1.0)
-plt.xticks(pd.date_range(min(dates), max(dates), freq='1M'), rotation=45)  # For example, every 2 weeks]
+# Split the data into three segments
+total_length = len(instantaneous_win_rates)
+last_third_start = total_length // 3 * 2
+last_two_thirds_start = total_length // 3
+
+# Plotting for entire dataset
+fig, ax1 = plt.subplots(figsize=(10, 6))
+ax1.plot(dates, cumulative_rates, color='blue')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Profitability Ratio', color='blue')
+ax1.tick_params('y', colors='blue')
+ax1.set_title('Profitability Ratio and Cumulative Profit Over Time - Entire Dataset')
+ax1.xaxis.set_major_locator(plt.MaxNLocator(10))
+ax1.xaxis.set_minor_locator(plt.MaxNLocator(50))
+
+ax2 = ax1.twinx()
+ax2.plot(dates_profit, profits, color='red')
+ax2.set_ylabel('Cumulative Profit', color='red')
+ax2.tick_params('y', colors='red')
+
+plt.tight_layout()
+plt.show()
+
+# Plotting for the last third of data
+fig, ax1 = plt.subplots(figsize=(10, 6))
+ax1.plot(dates[last_third_start:], cumulative_rates[last_third_start:], color='blue')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Profitability Ratio', color='blue')
+ax1.tick_params('y', colors='blue')
+ax1.set_title('Profitability Ratio and Cumulative Profit Over Time - Last Third')
+ax1.xaxis.set_major_locator(plt.MaxNLocator(10))
+ax1.xaxis.set_minor_locator(plt.MaxNLocator(50))
+
+ax2 = ax1.twinx()
+ax2.plot(dates_profit[last_third_start:], profits[last_third_start:], color='red')
+ax2.set_ylabel('Cumulative Profit', color='red')
+ax2.tick_params('y', colors='red')
+
+plt.tight_layout()
+plt.show()
+
+# Plotting for the last two-thirds of data
+fig, ax1 = plt.subplots(figsize=(10, 6))
+ax1.plot(dates[last_two_thirds_start:], cumulative_rates[last_two_thirds_start:], color='blue')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Profitability Ratio', color='blue')
+ax1.tick_params('y', colors='blue')
+ax1.set_title('Profitability Ratio and Cumulative Profit Over Time - Last Two-Thirds')
+ax1.xaxis.set_major_locator(plt.MaxNLocator(10))
+ax1.xaxis.set_minor_locator(plt.MaxNLocator(50))
+
+ax2 = ax1.twinx()
+ax2.plot(dates_profit[last_two_thirds_start:], profits[last_two_thirds_start:], color='red')
+ax2.set_ylabel('Cumulative Profit', color='red')
+ax2.tick_params('y', colors='red')
+
 plt.tight_layout()
 plt.show()
