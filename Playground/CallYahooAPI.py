d427eaf8e7e0e00273c3bd13e5948a4535cf4f88
skimcloud <113874029+skimcloud@users.noreply.github.com>
2023-12-21 02:33:22 -0800

    ⌛ Final Dataset Coming... (DataMerge.py)
    Requirements in DataMerge.py

--- Playground/CallYahooAPI.py
+++ Playground/CallYahooAPI.py
@@ -8,8 +8,7 @@ if not os.path.exists('Playground/raw_index_data'):
 
 
 # Iterate through unique tickers
-#unique_tickers = ['^VIX', '^VVIX', '^RVX', 'HYG', 'SPY', '^SKEW', 'GLD', 'HYG', 'QQQ', 'EURUSD=X', 'ES=F', 'YM=F', 'UVXY', '^MXX', '^XAX', '^GDAXI', '^BUK100P', '^KS11', 'LE=F', 'GBPUSD=X', 'BTC-USD', '^N225', 'JPY=X', '^SPX', '^DJI', '^NYA', '^IXIC', '^NDX', '^RUI', '^RUT', '^RUA', '^GSPC', '^GSPTSE', '^FTSE', '^FTMC', '^FTAS', 'DAX', '^AFLI', '^AORD', '^HSI', '^STI', '^KLSE', '^NSEI', '^NSMIDCP', '^CRSLDX', 'GC=F', 'CL=F', 'HO=F', 'BZ=F', 'BOIL', 'QM=F', 'NG=F', 'LE=F', '^TNX', 'UGA', 'CT=F', 'RB=F', 'ZW=F', 'SI=F', 'PL=F', 'HG=F', 'ZC=F']
-unique_tickers = ['^VIX', '^VVIX']
+unique_tickers = ['^VIX', '^VVIX', '^RVX', 'HYG', 'SPY', '^SKEW', 'GLD', 'HYG', 'QQQ', 'EURUSD=X', 'ES=F', 'YM=F', 'UVXY', '^MXX', '^XAX', '^GDAXI', '^BUK100P', '^KS11', 'LE=F', 'GBPUSD=X', 'BTC-USD', '^N225', 'JPY=X', '^SPX', '^DJI', '^NYA', '^IXIC', '^NDX', '^RUI', '^RUT', '^RUA', '^GSPC', '^GSPTSE', '^FTSE', '^FTMC', '^FTAS', 'DAX', '^AFLI', '^AORD', '^HSI', '^STI', '^KLSE', '^NSEI', '^NSMIDCP', '^CRSLDX', 'GC=F', 'CL=F', 'HO=F', 'BZ=F', 'BOIL', 'QM=F', 'NG=F', 'LE=F', '^TNX', 'UGA', 'CT=F', 'RB=F', 'ZW=F', 'SI=F', 'PL=F', 'HG=F', 'ZC=F']
 for ticker in unique_tickers:
     # Specify the date range
     start_date = '2019-11-18'
---
+++ Playground/DataMerge.py
@@ -0,0 +1,10 @@
+# Requirements
+# From Stationary_Stock_Data: Get PDF parameters and arima coefficients csv info
+# From Stock_With_Features_Data: Get All Columns (Technical Indicators)
+# From Stationary_Index_Data: Get PDF parameters and arima coefficients csv info
+# From Index_With_Features_data: Combine all columns from all csv's using timestamp
+# Calculate and Add triple barrier data.
+
+# Calculate log20_DiffMean
+# Calculate log5_DiffMean
+# Calculate log5_ContractDiff
