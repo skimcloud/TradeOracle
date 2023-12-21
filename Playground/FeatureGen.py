2c7bf0f4d70006aff1ecf7ef5078bbfd9a1e8fb1
skimcloud <113874029+skimcloud@users.noreply.github.com>
2023-12-20 21:56:35 -0800

    New Columns in Stock/Index Data

--- Playground/FeatureGen.py
+++ Playground/FeatureGen.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 
 # Create a new directory to store the output files
-output_directory = 'Playground/Processed_Index_Data'
+output_directory = 'Playground/Processed_Stock_Data'
 os.makedirs(output_directory, exist_ok=True)
 
 # Function to calculate indicators
 def calculate_indicators(file_path):
     data = pd.read_csv(file_path)
-
+    ticker = file_path.split('\\')[-1].split('_')[0]
     # Set Date as the index if it's not already set
     if 'Date' in data.columns:
         data['Date'] = pd.to_datetime(data['Date'])
@@ -34,8 +34,9 @@ def calculate_indicators(file_path):
     data['avgVolume_60'] = data['Volume'].rolling(window=60, min_periods=60).mean()
     data['avgVolume_250'] = data['Volume'].rolling(window=250, min_periods=250).mean()
 
-    # Log Delta
-    data['logDayRet'] = np.log(data['Adj Close'] / data['Open'])
+    
+    # Calculating the single-day logarithmic return involves taking the logarithm of close / open
+    data['log1DayRet'] = np.log(data['Adj Close'] / data['Open'])
     data['logMA5_20'] = np.log(data['MA_5'] / data['MA_20'])
     data['logMA20_250'] = np.log(data['MA_20'] / data['MA_250'])
     data['logMA5_250'] = np.log(data['MA_5'] / data['MA_250'])
@@ -43,18 +44,29 @@ def calculate_indicators(file_path):
     # Calculate the absolute difference between 'High' and 'Low'
     absolute_diff = np.abs(data['High'] - data['Low'])
 
-    # Apply the logarithm to the absolute difference (adding a small constant epsilon)
-    data['logDayRange_20'] = np.log(absolute_diff)
+    # Apply the logarithm to the absolute difference 
+    data['log1DayRange'] = np.log(absolute_diff)
 
     # Calculate HL_20_realVol_20 or any other desired calculations
-    data['HL_20_realVol_20'] = data['logDayRange_20'].rolling(window=20).std()
+    data['HL_20_realVol_20'] = data['log1DayRange'].rolling(window=20).std()
+    data['HL_5_realVol_20'] = data['log1DayRange'].rolling(window=5).std()
 
+    # Calculate two-day logarithmic difference
+    absolute_diffD2 = np.abs(data['Adj Close'] - data['Adj Close'].shift(2))
+    data['logD2'] = np.log(absolute_diffD2)
+
+    # Calculate five-day logarithmic difference
+    absolute_diffD5 = np.abs(data['Adj Close'] - data['Adj Close'].shift(5))
+    data['logD5'] = np.log(absolute_diffD5)
+
+    prefix = f'{ticker}_'
+    data.columns = [prefix + col for col in data.columns]
     # Saving the updated data to a new CSV file
     output_file = os.path.join(output_directory, os.path.basename(file_path))
     data.to_csv(output_file, index=True)
 
 # Define the directory containing the files
-directory = 'Playground/stationary_index_data'
+directory = 'Playground/stationary_stock_data'
 
 # Get all files ending with 'prices.csv' in the specified directory
 files = [f for f in os.listdir(directory) if f.endswith('prices.csv')]
